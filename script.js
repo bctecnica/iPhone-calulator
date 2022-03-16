8ec1a638a48e33d94107ea81264303b610bb86f6
ben <jammydude7@googlemail.com>
2022-03-16 14:06:49 +0000

    added js selectors for elements

---
+++ script.js
@@ -0,0 +1,38 @@
+// DOM element selectors
+// calc functions
+const ac = document.querySelector('.ac');
+const pm = document.querySelector('.p-m');
+const percent  = document.querySelector('.percent');
+// calc numbers
+const decimal = document.querySelector('.decimal');
+const number0 = document.querySelector('.number-0');
+const number1 = document.querySelector('.number-1');
+const number2 = document.querySelector('.number-2');
+const number3 = document.querySelector('.number-3');
+const number4 = document.querySelector('.number-4');
+const number5 = document.querySelector('.number-5');
+const number6 = document.querySelector('.number-6');
+const number7 = document.querySelector('.number-7');
+const number8 = document.querySelector('.number-8');
+const number9 = document.querySelector('.number-9');
+const numberElArray = [
+  number0, number1, number2, number3, number4,
+  number5, number6, number7, number8, number9
+];
+// calc operations
+const addition = document.querySelector('.addition');
+const subtraction = document.querySelector('.subtraction');
+const multiplication = document.querySelector('.multiplication');
+const division = document.querySelector('.division');
+const equal = document.querySelector('.equal');
+// calc landscape functions
+const ozToL = document.querySelector('.oz-to-l');
+const pi = document.querySelector('.pi');
+const stoneToKg = document.querySelector('.stone-to-kg');
+const Square = document.querySelector('.square');
+const inchToCm = document.querySelector('.inch-to-cm');
+const squareRoot = document.querySelector('.square-root');
+const kphToMph = document.querySelector('.kph-to-mph');
+const cubed = document.querySelector('.cubed');
+const farToCel = document.querySelector('.far-to-cel');
+const random = document.querySelector('.random');
